e61dcf0b8a38f03c09e10d0231c17218bd8ae142
BenR <thisbenrogers@gmail.com>
2019-06-18 12:35:21 -0500

    refactor for seperating reducers

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import reduceTodos from './reducers';
+
 import App from './components/App';
 
-// const store = createStore();
+const store = createStore(reduceTodos);
 
 ReactDOM.render(
-  // <Provider>
-  <App />,
-  // </Provider>,
+  <Provider>
+    <App />,
+  </Provider>,
   document.getElementById('root')
 );
